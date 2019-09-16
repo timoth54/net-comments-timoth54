2d07be82a65eaac4c2b129305ef8bc9a42bc737b
Timothy Tucker <timoth54@users.campus>
2019-09-16 14:22:41 -0500

    Participation submission

--- src/Vector3.cs
+++ src/Vector3.cs
@@ -47,5 +47,31 @@ namespace VectorMath
             this.Y = y;
             this.Z = z;
         }
+
+        /// <summary>
+        /// Gets the magnitude of this vector.
+        /// </summary>
+        public double Magnitude
+        {
+            get
+            {
+                return Math.Sqrt(this.X * this.X + this.Y * this.Y + this.Z * this.Z);
+            }
+        }
+
+        /// <summary>
+        /// Calculates the cross product of this and
+        /// the other vector (maybe?).
+        /// </summary>
+        /// <param name="other">The vector to cross with this vector.</param>
+        /// <returns>The cross product.</returns>
+        public Vector3 CrossProduct(Vector3 other)
+        {
+            return new Vector3(
+                this.Y * other.Z - this.Z * other.Y,
+                this.Z * other.X - this.X * other.Z,
+                this.X * other.Y - this.Y * other.X
+            );
+        }
     }
 }
